1080cbe6bb31f043e21f2e336c6faf6e97b0be25
PabloVisualGrowth <pabloperez@visualandgrowth.com>
2026-04-09 15:28:30 +0200

    Revert: Index.tsx back to pre-fullpage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,40 +5,33 @@ import MortgageCalculator from "@/components/MortgageCalculator";
 import About from "@/components/About";
 import Storytelling from "@/components/Storytelling";
 import Blog from "@/components/Blog";
+import Team from "@/components/Team";
 import ContactCTA from "@/components/ContactCTA";
 import Footer from "@/components/Footer";
 import CookieConsent from "@/components/CookieConsent";
 import LeadModal from "@/components/LeadModal";
-import Team from "@/components/Team";
-import { FullPage } from "@/components/FullPage";
-import PageNav from "@/components/PageNav";
+import ScrollNav from "@/components/ScrollNav";
 
-const SECTION_IDS = [
-  "inicio", "servicios", "calculadora", "como-trabajamos",
-  "nosotros", "equipo", "blog", "contacto",
-];
-
-const Index = () => (
-  <>
-    <Header />
-    <PageNav />
-    <FullPage sectionIds={SECTION_IDS}>
-      {/* Each direct child = one page = 100vh */}
-      <div style={{ height: "100vh", overflow: "hidden" }}><Hero /></div>
-      <div style={{ height: "100vh", overflow: "hidden auto" }}><Services /></div>
-      <div style={{ height: "100vh", overflow: "hidden auto" }}><MortgageCalculator /></div>
-      <div style={{ height: "100vh", overflow: "hidden auto" }}><Storytelling /></div>
-      <div style={{ height: "100vh", overflow: "hidden auto" }}><About /></div>
-      <div style={{ height: "100vh", overflow: "hidden auto" }}><Team /></div>
-      <div style={{ height: "100vh", overflow: "hidden auto" }}><Blog /></div>
-      <div style={{ height: "100vh", overflow: "hidden auto" }}>
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <ScrollNav />
+      <main>
+        <Hero />
+        <Services />
+        <MortgageCalculator />
+        <Storytelling />
+        <About />
+        <Team />
+        <Blog />
         <ContactCTA />
-        <Footer />
-      </div>
-    </FullPage>
-    <CookieConsent />
-    <LeadModal />
-  </>
-);
+      </main>
+      <Footer />
+      <CookieConsent />
+      <LeadModal />
+    </>
+  );
+};
 
 export default Index;
